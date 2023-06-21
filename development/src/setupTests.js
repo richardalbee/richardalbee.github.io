98ca2fb927ed143386b2d00a47de119a5246f4bc
ralbee1 <93877353+richardalbee@users.noreply.github.com>
2023-06-20 21:54:36 -0500

    Updating Node.js tests for CI/CD workflow

--- development/src/setupTests.js
+++ development/src/setupTests.js
@@ -2,4 +2,17 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+//import '@testing-library/jest-dom';
+//import Slider from "react-slick";
+import "@testing-library/jest-dom/extend-expect";
+
+//Fix for "matchMedia not present, legacy browsers require a polyfill jest" error
+window.matchMedia =
+  window.matchMedia ||
+  function() {
+    return {
+      matches: false,
+      addListener: function() {},
+      removeListener: function() {}
+    };
+  };
